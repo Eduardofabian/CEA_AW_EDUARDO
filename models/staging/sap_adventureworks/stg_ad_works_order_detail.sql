ab9e1cd88dee6c38ba37c4476bd6988976329843
EDUARDO DE OLIVEIRA <eduardof_oliveira@hotmail.com>
2025-12-24 01:56:56 +0000

    stg_order_detail

--- models/staging/sap_adventureworks/stg_ad_works_order_detail.sql
+++ models/staging/sap_adventureworks/stg_ad_works_order_detail.sql
@@ -5,14 +5,14 @@ with source as (
 , renamed as (
     select
         -- PK
-        {{ dbt_utils.generate_surrogate_key(['salesorderid']) }} as customer_sk
+        {{ dbt_utils.generate_surrogate_key(['salesorderid', 'salesorderdetailid']) }} as sales_order_detail_sk
         , cast(salesorderid as int) as sales_order_id
         , cast(salesorderdetailid as int) as order_detail_id
         , cast(orderqty as int) as order_qty
         , cast(productid as int) as product_id
         , cast(specialofferid as int) as specialoffer_id
-        , cast(unitprice as int) as unitprice
-        , cast(unitpricediscount as int) as unit_price_discount
+        , cast(unitprice as float) as unit_price
+        , cast(unitpricediscount as float) as unit_price_discount
         , cast(modifieddate as timestamp) as modified_date
     from source
 )
