9859768917d172a84ca02d0e6b215a3fbc11cb82
EDUARDO DE OLIVEIRA <eduardof_oliveira@hotmail.com>
2025-12-24 01:59:51 +0000

    int_sales

--- models/intermidiate/int_sales_enriched.sql
+++ models/intermidiate/int_sales_enriched.sql
@@ -5,9 +5,10 @@ with order_header as (
 , order_detail as (
     select *
     from {{ref('stg_ad_works_sales_order_detail')}}
-
-
-
 )
-select * 
-from order_header
+, joined as (
+    select *
+    from order_header
+    left join order_detail
+        on order_header.sales_order_id = order_detail.sales_order_id
+)
