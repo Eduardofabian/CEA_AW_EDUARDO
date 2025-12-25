078e8c0b044af1cce2027367debbe55a6560d492
EDUARDO DE OLIVEIRA <eduardof_oliveira@hotmail.com>
2025-12-25 20:46:56 +0000

    int_Sales

--- models/intermidiate/int_sales_enriched.sql
+++ models/intermidiate/int_sales_enriched.sql
@@ -1,5 +1,13 @@
 with order_header as (
-    select *
+    select 
+        sales_order_id
+        ,customer_id       -- Vai virar fk_customer na Fato
+        ,salesperson_id   -- Vai virar fk_employee na Fato
+        ,bill_to_address_id -- Vai virar fk_location na Fato
+        ,credit_card_id,    -- Vai virar fk_credit_card na Fato
+        ,order_date
+        ,ship_date
+        status
     from {{ref('stg_ad_works_salesorderheader')}}
 )
 , order_detail as (
