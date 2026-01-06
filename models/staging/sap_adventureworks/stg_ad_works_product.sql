8b5426608f15e4601c0135288a3ba347dc45648a
EDUARDO DE OLIVEIRA <eduardof_oliveira@hotmail.com>
2026-01-06 01:48:14 +0000

    att yml

--- models/staging/sap_adventureworks/stg_ad_works_product.sql
+++ models/staging/sap_adventureworks/stg_ad_works_product.sql
@@ -1,22 +1,29 @@
 with source as (
-    select * from {{ source('ad_works', 'production_product') }}
+    select *
+    from {{ source('ad_works', 'production_product') }}
 )
 
 , renamed as (
     select
-        -- PK
         {{ dbt_utils.generate_surrogate_key(['productid']) }} as product_sk
         , cast(productid as int) as product_id
+        , cast(productsubcategoryid as int) as product_subcategory_id
         , cast(name as string) as product_name
         , cast(productnumber as string) as product_number
-        , cast(coalesce(color, 'No Color') as string) as color
-        , cast(coalesce(class, 'No Class') as string) as class_name
-        , cast(coalesce(style, 'No Style') as string) as style_name
-        , cast(standardcost as float) as standard_cost
-        , cast(listprice as float) as list_price
-        , cast(reorderpoint as int) as reorder_point
+        , cast(color as string) as color
         , cast(safetystocklevel as int) as safety_stock_level
-        , cast(productsubcategoryid as int) as product_subcategory_id
+        , cast(reorderpoint as int) as reorder_point
+        , cast(standardcost as numeric) as standard_cost
+        , cast(listprice as numeric) as list_price
+        , cast(size as string) as size
+        , cast(weight as numeric) as weight
+        , cast(daystomanufacture as int) as days_to_manufacture
+        , cast(productline as string) as product_line
+        , cast(class as string) as class_name
+        , cast(style as string) as style_name
+        , cast(sellstartdate as timestamp) as sell_start_date
+        , cast(sellenddate as timestamp) as sell_end_date
+        
         , cast(modifieddate as timestamp) as modified_date
     from source
 )
